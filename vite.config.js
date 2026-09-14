50e46f66894db0deee36207a97c2b5525d8e439d
Kimberley Wong <kimberley.wpn@gmail.com>
2026-09-14 16:36:54 +0800

    Second commit test

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8080', 
         changeOrigin: true,
         configure(proxy) {
           proxy.on('proxyReq', proxyRequest => proxyRequest.setHeader('origin', 'http://localhost:8080'));
